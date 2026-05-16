bae5a574595c541736f9ebabbab457178888531d
Vignesh <ssvignesh.619@gmail.com>
2026-05-16 10:06:40 +0530

    Initial Commit

---
+++ wallet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wallet
+//
+//  Created by vignesh on 16/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wallet/walletApp.swift
@@ -0,0 +1,17 @@
+//
+//  walletApp.swift
+//  wallet
+//
+//  Created by vignesh on 16/05/26.
+//
+
+import SwiftUI
+
+@main
+struct walletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
